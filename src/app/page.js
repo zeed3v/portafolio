4b836711345de6167eef1f26d3b201ab42eb333c
Felipe Briones <98282880+zeed3v@users.noreply.github.com>
2023-06-01 20:13:04 -0400

    Add files via upload

---
+++ src/app/page.js
@@ -0,0 +1,149 @@
+"use client";
+
+import Head from 'next/head'
+import {BsFillMoonStarsFill} from 'react-icons/bs'
+import {AiFillLinkedin, AiFillGithub} from 'react-icons/ai'
+import Image from "next/image";
+import deved from '/public/dev-ed-wave.png';
+import code from '/public/code.png';
+import guion from '/public/guion.jpg';
+import web1 from '/public/web1.jpg';
+import web2 from '/public/web2.png';
+import web3 from '/public/web3.jpg';
+import web4 from '/public/web4.png';
+import web5 from '/public/web5.jpg';
+import web6 from '/public/web6.jpg';
+import { useState } from 'react';
+
+export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
+  return (
+    <div className={darkMode ? "dark" : ""}>
+      
+      <Head>
+        <title>Portafolio</title>
+        <meta name="description" content="Portafolio" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className='bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900'>
+        <section className='min-h-screen'>
+          <nav className='py-10 mb-12 flex justify-between'>
+            <h1 className='text-3xl font-burtons'>Portafolio</h1>
+            <ul className='flex items-center'>
+              <li>
+                <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className='cursor-pointer text-2xl'/>
+              </li>
+              <li>
+                <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"href="#">Currículum</a>
+              </li>
+            </ul>
+
+          </nav>
+          <div className='text-center p-10  dark:text-white'>
+            <h2 className='text-5xl py-2 text-teal-600 font-medium md:text-6xl'>
+              Felipe Briones
+            </h2>
+            <h3 className='text-2xl py-2 md:text-3xl'>
+              Programador y guionista
+            </h3>
+            <p className='text-medium py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto dark:text-white'>Programador entusiasta en búsqueda de una oportunidad para trabajar en el mundo de 
+              del desarrollo. Me gusta aprender nuevos lenguajes y trabajar en equipo.
+            </p>
+          </div>
+          <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600'>
+            <AiFillLinkedin />
+            <AiFillGithub />
+          </div>
+          <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96'>
+            <Image src={deved} layout='fill' objectFit="cover" />
+          </div>
+        </section>
+
+        <section>
+          <div className=' dark:text-white'>
+            <h3 className='text-3xl py-1'>Capacidades técnicas</h3>
+            <p className='text-md py-2 leading-8 text-gray-800 dark:text-white'>
+              Desde el comienzo de la historia ipsom loroipsom loroipsom loroipsom loro
+              ipsom loroipsom loroipsom loroipsom loroipsom loroipsom loro
+              <span className='text-teal-500'> Devsafío </span>ipsom loro
+              <span className='text-teal-500'> Empresas </span>ipsom loro ipsom loro
+              ipsom loro ipsom loro ipsom loro ipsom loro ipsom loro.
+            </p>
+            <p className='text-md py-2 leading-8 text-gray-800 dark:text-white'>
+              Desde el comienzo de la historia ipsom loro ipsom loro ipsom loro ipsom loro
+              ipsom loro ipsom loro ipsom loro ipsom loro ipsom loro ipsom loro
+            </p>
+          </div>
+          <div className='lg:flex gap-10'>
+            <div className='text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white'>
+              <Image src={code} width={100} height={100} className='mx-auto'/>
+              <h3 className='text-2lg font-medium pt-8 pb-2 '>
+                Código
+              </h3>
+              <p className='py-2'>
+                Desde el comienzo de la historia ipsom loro 
+                ipsom loro ipsom loro ipsom loro
+              </p>
+              <h4 className='py-4 text-teal-600'>Conocimientos</h4>
+              <p className='text-gray-800 py-1'>JavaScript</p>
+              <p className='text-gray-800 py-1'>C#</p>
+              <p className='text-gray-800 py-1'>Frameworks FE</p>
+            </div>
+            <div className='text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white'>
+              <Image src={guion} width={100} height={100} className='mx-auto'/>
+              <h3 className='text-2lg font-medium pt-8 pb-2'>
+                Guion
+              </h3>
+              <p className='py-2'>
+                Desde el comienzo de la historia ipsom loro 
+                ipsom loro ipsom loro ipsom loro
+              </p>
+              <h4 className='py-4 text-teal-600'>Conocimientos</h4>
+              <p className='text-gray-800 py-1'>Series</p>
+              <p className='text-gray-800 py-1'>Cine</p>
+              <p className='text-gray-800 py-1'>Teleseries</p>
+            </div>
+            
+          </div>
+        </section>
+        <section>
+          <div>
+            <h3 className='text-3xl py-1 dark:text-white'>Portafolio</h3>
+            <p className='text-md py-2 leading-8 text-gray-800 dark:text-white'>
+              Desde el comienzo de la historia ipsom loroipsom loroipsom loroipsom loro
+              ipsom loroipsom loroipsom loroipsom loroipsom loroipsom loro
+              <span className='text-teal-500'> Devsafío </span>ipsom loro
+              <span className='text-teal-500'> Empresas </span>ipsom loro ipsom loro
+              ipsom loro ipsom loro ipsom loro ipsom loro ipsom loro.
+            </p>
+            <p className='text-md py-2 leading-8 text-gray-800 dark:text-white'>
+              Desde el comienzo de la historia ipsom loro ipsom loro ipsom loro ipsom loro
+              ipsom loro ipsom loro ipsom loro ipsom loro ipsom loro ipsom loro
+            </p>
+          </div>
+          <div className='flex flex-col py-10 lg:flex-row lg:flex-wrap'>
+            <div className='basis-1/3 flex-1'>
+              <Image src={web1} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive'/>
+            </div>
+            <div className='basis-1/3 flex-1'>
+              <Image src={web2} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive'/>
+            </div>
+            <div className='basis-1/3 flex-1'>
+              <Image src={web3} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive'/>
+            </div>
+            <div className='basis-1/3 flex-1'>
+              <Image src={web4} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive'/>
+            </div>
+            <div className='basis-1/3 flex-1'>
+              <Image src={web5} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive'/>
+            </div>
+            <div className='basis-1/3 flex-1'>
+              <Image src={web6} className='rounded-lg object-cover' width={'100%'} height={'100%'} layout='responsive'/>
+            </div>
+          </div>
+        </section>    
+      </main>
+    </div>
+  )
+}
